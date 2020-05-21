6a6591659f3b75db978e17389d5908923692e21f
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-21 10:05:28 -0300

    server config

---
+++ backend/src/config/server.js
@@ -0,0 +1,16 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+
+//it's middleware to use boyParser
+server.use(bodyParser.urlencoded({ extended: true }))
+//it's middleware to use json
+server.use(bodyParser.json())
+
+//express listen on port defined at line 1
+server.listen(port, function() {
+    console.log(`The backend is run on port ${port}`)
+})
