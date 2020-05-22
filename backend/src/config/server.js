234f3ec3aa4e51c7bf862b36840e7ab457831eb8
Rharuow <haryssonsoares@gmail.com>
2020-05-22 09:35:09 -0300

    cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,12 +3,15 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 
 //it's middleware to use boyParser
 server.use(bodyParser.urlencoded({ extended: true }))
 //it's middleware to use json
 server.use(bodyParser.json())
+//aplly middleware cors
+server.use(allowCors)
 
 //express listen on port defined at line 1
 server.listen(port, function() {
