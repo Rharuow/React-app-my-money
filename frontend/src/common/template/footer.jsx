6e420c67950507a5ea835a716a6ba626d912410b
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-25 11:27:05 -0300

    footer componenet

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; Harysson Soares 2019
+            <a href="https://github.com/Rharuow" target="_blank">Rharuow.</a>
+        </strong>
+    </footer>
+)
