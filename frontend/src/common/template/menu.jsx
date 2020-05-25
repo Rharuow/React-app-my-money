b057f5da7b74ee1ecc478ff181928d14874fd292
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-25 11:17:51 -0300

    menutree component

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
 export default props => (
     <ul className="sidebar-menu">
-
+        <MenuItem path='#' label="dashboard" icon="dashboard"/>
+        <MenuTree label="Cadastro" icon="edit">
+            <MenuItem path="#billingCycles" label="Ciclos de pagamenos" icon="usd"/>
+        </MenuTree>
     </ul>
 )
