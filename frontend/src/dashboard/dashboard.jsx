50dcc25c6a28d3e2ca0fb2b758fa90f5e6f697f2
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-25 12:10:27 -0300

    content and contentHeader component

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default props => (
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+const Dashboard = props => (
     <div>
-        <h1>Dashboard</h1>
+        <ContentHeader title="Dashboard" small="Versão 1.0"/>
+        <Content>
+            Dashboard
+        </Content>
     </div>
 )
+
+export default Dashboard
