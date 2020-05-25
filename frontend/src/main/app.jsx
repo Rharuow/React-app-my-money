c47d14a7d763c411e5258b0f9873d556e49b2b88
HARYSSON SOARES RODRIGUES DE QUEIROZ <h.queiroz@escolar.ifrn.edu.br>
2020-05-25 11:48:21 -0300

    routes components

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,13 +5,14 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
+import Routers from './routes'
 
 export default props => (
     <div className="wrapper">
         <Header/>
         <SideBar/>
         <div className="content-wrapper">
-            <h1>Content</h1>
+            <Routers/>
         </div>
         <Footer/>
     </div>
